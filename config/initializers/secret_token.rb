ec1617a72fb56be30421eacd0305e914040e8366
Susan Holcomb <thefurtheroff@gmail.com>
2013-10-30 16:05:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Physicsninja::Application.config.secret_token = 'b1f006f8ff2209f1a8cd6c67c09397bcaf07af68244dc8103c04dcefd3b992ac25b22175b154cbe614d68a4e64c84c573b89f3c1b87ccd4c71d1a51a098c9a3b'

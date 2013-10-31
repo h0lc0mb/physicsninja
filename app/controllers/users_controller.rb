0607aec9fc25b116fdcd584cfbb1fd99649269d4
Susan Holcomb <thefurtheroff@gmail.com>
2013-10-30 21:11:59 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+      #sign_in @user
+  		flash[:success] = "The Physics Ninja welcomes you."
+  		redirect_to root_path
+  	else
+  		render 'new'
+  	end
   end
 end

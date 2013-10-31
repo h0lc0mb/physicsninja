62a9fd4ea1c704e1c1c4ebb9928004981fe1b6db
Susan Holcomb <thefurtheroff@gmail.com>
2013-10-30 20:20:31 -0700

    Make a basic User model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :username, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  NO_SPACES_REGEX = /^[\S]+$/
+  validates :username, presence: true, length: { maximum: 50 }, 
+  																 format: { with: NO_SPACES_REGEX, 
+  																					 message: "username can't contain spaces" }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  									uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end 
